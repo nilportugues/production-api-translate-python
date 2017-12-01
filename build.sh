efb954be857ff60d1777dd1a1946de8891ae9127
Nil Portugués Calderó <nil@trovit.com>
2017-12-01 15:36:49 +0100

    build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+mkdir -p build
+# Git clone the project
+git clone git@github.com:nilportugues/production-api-translate-python.git build
+ 
+# Generate release tarball
+cd build/src/
+python setup.py sdist
+cd ..
+cd ..
+
+# save somewhere the build at: 
+mv ./build/src/dist/translate_api-0.1.tar.gz ./
+
+rm -rf .build
